afe4e0e803709a0d410c7e8b6772b611637638e5
Alejandro CR <contacto@alejandrocr.co>
2024-02-03 16:34:37 -0500

    remove unused urls

--- source/persons/urls.js
+++ source/persons/urls.js
@@ -14,9 +14,5 @@ module.exports = {
   media: '/media/persons',
   index: baseUrl + '/',
   model: baseUrl + '/:slug',
-  modelEdit: baseUrl + '/:slug/edit',
-  profile: baseUrl + '/profile',
-  profileEdit: baseUrl + '/profile/edit',
   register: baseUrl + '/register',
-  relatedVideos: baseUrl + '/:slug/videos',
 };
